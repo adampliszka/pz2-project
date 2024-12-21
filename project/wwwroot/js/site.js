c3abfe1d6495fea7cef7607a02ac3a96455f6403
adampliszka <92460299+adampliszka@users.noreply.github.com>
2024-12-21 17:41:43 +0100

    Add files via upload

--- project/wwwroot/js/site.js
+++ project/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+/*function updateTheme() {
+    const colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ?
+        "dark" :
+        "light";
+    document.querySelector("html").setAttribute("data-bs-theme", colorMode);
+    document.querySelector("nav").classList.add("navbar-" + colorMode);
+}
+
+updateTheme()
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)*/
